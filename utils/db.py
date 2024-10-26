90cd0b4142ebac2f16ba22c30b0ca0fbc021b7c5
UstozCoder <SolihCoder@gmail.com>
2024-10-26 13:44:42 +0500

    some more changes

--- utils/db.py
+++ utils/db.py
@@ -10,3 +10,25 @@ def create_connection(host="localhost", user="root", password="mysql", database=
     except Exception as e:
         print(f"Error creating database connection: {e}")
         raise pymysql.Error(e)
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Exception as e:
+        print(f"The error '{e}' occurred")
+        raise pymysql.Error(e)
+
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print(f"The error '{e}' occurred")
+        raise pymysql.Error(e)
